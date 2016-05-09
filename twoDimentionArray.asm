12ec54ceb6326cd06cf3053a293d5dcf2efe6a36
taimoor_t <taimoor_t@yahoo.com>
2016-05-09 10:26:00 +0500

    column addition in 2d array

--- twoDimentionArray.asm
+++ twoDimentionArray.asm
@@ -1,14 +1,14 @@
 .data
 	int_arr: .word 0 1 2 3
-		 .word 0 1 2 3
-		 .word 0 1 2 3
-		 .word 0 1 2 3
-		 .word 0 1 2 3
-		 .word 0 1 2 3
-		 .word 0 1 2 3
-		 .word 0 1 2 3
-		 .word 0 1 2 3
-		 .word 0 1 2 3
+			 .word 0 1 2 3  
+			 .word 0 1 2 3  
+			 .word 0 1 2 3
+			 .word 0 1 2 3
+			 .word 0 1 2 3
+			 .word 0 1 2 3
+			 .word 0 1 2 3
+			 .word 0 1 2 3
+			 .word 0 1 2 3
 
 	newLine: .asciiz "\n"
 	
@@ -17,21 +17,31 @@
 main:
 
 	#registers
+	#$t0 = i
+	#$t1 = length
+	#$t2 = sum of 2nd column
+	#$t3 = temp value
+	
 	
 	li $t1, 40
 	la $a1, int_arr
+	addi $a1, $a1, 4
 	
 	loop:
-	beq $t0, $t1, end
-		
-		li $v0, 1
-		lw $a0, ($a1)
-		syscall
-				
-		add $t0, $t0, 1
-		add $a1, $a1, 4
-		
-	j loop
-end:	
+	beq $t0, 10, end
+		lw $t3, ($a1)
+
+		add $t2, $t2, $t3
+		addi $a1, $a1, 16
+		addi $t0, $t0, 1
+			j loop
+
+			
+end:
+li $v0, 1
+move $a0, $t2
+syscall
+
+
 li $v0, 10
 syscall
